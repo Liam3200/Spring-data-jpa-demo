1a1260d2c119720eced823a8e1a0e5ef384d6df6
Liam Greene <wgreene@guilford.edu>
2023-07-24 13:53:38 -0400

    Initial commit

---
+++ src/test/java/greene/jpademo/spring/data/jpademo/SpringDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package greene.jpademo.spring.data.jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
