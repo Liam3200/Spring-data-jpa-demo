96314a8ee0cbee4e5afa5901d5a419df908fa7f6
Liam Greene <wgreene@guilford.edu>
2023-07-26 13:46:43 -0400

    Created the Guardian Class

--- src/main/java/greene/jpademo/spring/data/jpademo/entity/Student.java
+++ src/main/java/greene/jpademo/spring/data/jpademo/entity/Student.java
@@ -1,6 +1,7 @@
 package greene.jpademo.spring.data.jpademo.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -38,7 +39,7 @@ public class Student {
     @Column(name="email_address"
             ,nullable = false)
     private String emailId;
-    private String guardianName;
-    private String guardianEmail;
-    private String guardianMobile;
+
+    @Embedded
+    private Guardian guardian;
 }
--- src/test/java/greene/jpademo/spring/data/jpademo/repository/StudentRepositoryTest.java
+++ src/test/java/greene/jpademo/spring/data/jpademo/repository/StudentRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import greene.jpademo.spring.data.jpademo.entity.Guardian;
 import greene.jpademo.spring.data.jpademo.entity.Student;
 
 
@@ -21,13 +22,32 @@ public class StudentRepositoryTest {
                 .firstName("John")
                 .lastName("Doe")
                 .emailId("john.doe@domain")
-                .guardianName("Jane Doe")
-                .guardianEmail("jane.doe@domain")
-                .guardianMobile("555-555-5555")
+                // .guardianName("Jane Doe")
+                // .guardianEmail("jane.doe@domain")
+                // .guardianMobile("555-555-5555")
                 .build();
         studentRepository.save(student);
     }
 
+    @Test
+    public void saveStudentWithGuardian() {
+        Guardian guardian = Guardian.builder()
+                .name("Jane Doe")
+                .email("jane.doe@domain")
+                .mobile("555-555-5555")
+                .build();
+
+        Student student = Student.builder().
+                firstName("John")
+                .lastName("Doe")
+                .emailId("john.doe@domain")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
+    }
+    
+    @Test
     public void printAllStudent() {
         List<Student> studentList = studentRepository.findAll();
 
